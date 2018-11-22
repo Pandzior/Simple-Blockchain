ea7faca3207f50c2b723bece6045b7f0e766f2ae
Marcin Jedrusiak <marcinjdr@gmail.com>
2018-11-22 09:56:30 +0100

    add Block class

--- src/com/marcinjdr/utils/Helper.java
+++ src/com/marcinjdr/utils/Helper.java
@@ -3,7 +3,6 @@ package com.marcinjdr.utils;
 import java.io.*;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.Base64;
 
 public class Helper {
 
@@ -14,11 +13,16 @@ public class Helper {
 
     public static String getSha256Hash(final Object object) throws NoSuchAlgorithmException, IOException {
         final MessageDigest digest = MessageDigest.getInstance(HASHING_ALGORITHM);
+        final StringBuilder builder = new StringBuilder();
 
         final byte[] bytes = objectToByteArray(object);
         final byte[] hash = digest.digest(bytes);
 
-        return Base64.getEncoder().encodeToString(hash);
+        for (byte b : hash) {
+            builder.append(String.format("%02x", b));
+        }
+
+        return builder.toString();
     }
 
     private static byte[] objectToByteArray(final Object object) throws IOException {
